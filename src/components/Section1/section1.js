d5b3e4e823fb3ce1fdab390d1a31645135d1f3a9
bogdan9876 <bogdankryveiko@gmail.com>
2023-10-30 18:49:10 +0200

    almost done

--- src/components/Section1/section1.js
+++ src/components/Section1/section1.js
@@ -1,12 +1,17 @@
 function Section1() {
   return (
     <div className="section1">
-      <img className="section1-image" src="images/section1.jpg" alt="welcome photo" width="400" height="250"/>
+      <img className="section1-image" src="images/section1.jpg" alt="welcome photo" width="600" height="400"/>
       <div className="section1-text">
         <h2 className="section1-text-title">Welcome to the store!</h2>
         <p className="section1-text-paragraph">
-          We open the door to a world of elegance and comfort, where the table will become the heart of your home. In our store, you will find exquisite tables for various styles and designs - from classic to modern, from wood to glass. We have the perfect table for every interior. Come to us and help us make your home even more elegant and cozy.
-        </p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+             culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </div>
   );
--- src/components/Section2/section2.js
+++ src/components/Section2/section2.js
@@ -1,29 +1,33 @@
 function Section2() {
-    return (
+  const items = [
+      {
+          image: "images/section2(1).jpg",
+          title: "Lamp1",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget est lorem ipsum dolor sit."
+      },
+      {
+          image: "images/section2(2).jpg",
+          title: "Lamp2",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget est lorem ipsum dolor sit."
+      },
+      {
+          image: "images/section2(3).jpg",
+          title: "Lamp3",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget est lorem ipsum dolor sit."
+      }
+  ];
+
+  return (
       <div className="section2">
-        <div className="section2-column1">
-          <img className="section2-image" src="images/section2(1).jpg" alt="lamp1" width="400" height="300"/>
-          <h3 className="section2-text-title">Lamp1</h3>
-          <p className="section2-text-paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget est lorem ipsum dolor sit.
-          </p>
-        </div>
-        <div className="section2-column2">
-          <img className="section2-image" src="images/section2(2).jpg" alt="lamp2" width="400" height="300"/>
-          <h3 className="section2-text-title">Lamp2</h3>
-          <p className="section2-text-paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget est lorem ipsum dolor sit.
-          </p>
-        </div>
-        <div className="section2-column3">
-          <img className="section2-image" src="images/section2(3).jpg" alt="lamp3" width="400" height="300"/>
-          <h3 className="section2-text-title">Lamp3</h3>
-          <p className="section2-text-paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget est lorem ipsum dolor sit.
-          </p>
-        </div>
+          {items.map((item, index) => (
+              <div className={`section2-column${index + 1}`} key={index}>
+                  <img className="section2-image" src={item.image} alt={`lamp${index + 1}`} width="400" height="300" />
+                  <h3 className="section2-text-title">{item.title}</h3>
+                  <p className="section2-text-paragraph">{item.description}</p>
+              </div>
+          ))}
       </div>
-    );
-  }
-  
-  export default Section2;
+  );
+}
+
+export default Section2;
