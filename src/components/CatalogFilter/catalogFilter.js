51a7b5e7f913e17a973f7a1507b314f712c641b4
bogdan9876 <bogdankryveiko@gmail.com>
2023-11-09 17:48:54 +0200

    a little bit fix filters

--- src/components/CatalogFilter/catalogFilter.js
+++ src/components/CatalogFilter/catalogFilter.js
@@ -3,21 +3,15 @@ import './CatalogFilter.css';
 
 const CatalogFilter = () => {
   return (
-    <div className="wrapper" style={{ borderRadius: '12px', maxWidth: '97%' }}>
-      <div
-        className="inner"
-        style={{
-          marginBottom: '1.4vw',
-          marginTop: '1.4vw',
-        }}
-      >
+    <div className="wrapper">
+      <div className="inner">
         <select className="selectSort" name="sort">
           <option style={{ display: 'none' }}>Choose option</option>
           <option value="sortByPrice">Sort by price</option>
           <option value="sortByTitle">Sort by title</option>
           <option value="sortByType">Sort by type</option>
         </select>
-        <div style={{ display: 'flex' }}>
+        <div>
           <label className="myLabel">
             Minimum price <input type="number" name="minPrice" />
           </label>
