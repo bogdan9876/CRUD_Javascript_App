94a2cae2f936acabfa63847d69536a2e6d9883c2
bogdan9876 <bogdankryveiko@gmail.com>
2023-11-11 14:30:08 +0200

    add some fix with item page

--- src/components/LampData/lampData.js
+++ src/components/LampData/lampData.js
@@ -3,28 +3,28 @@ const lamps = [
     id: 1,
     image: "images/section2(1).jpg",
     title: "Lamp1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: 1000
   },
   {
     id: 2,
     image: "images/section2(2).jpg",
     title: "Lamp2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: 1500
   },
   {
     id: 3,
     image: "images/section2(3).jpg",
     title: "Lamp3",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: 500
   },
   {
     id: 4,
     image: "images/section2(4).jpg",
     title: "Lamp4",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: 1200
   },
 ];
--- src/components/LampDetail/lampDetail.js
+++ src/components/LampDetail/lampDetail.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import lamps from '../LampData/lampData';
 import './lampDetail.css';
 
 function LampDetail() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const lamp = lamps.find((lamp) => lamp.id === parseInt(id));
 
   if (!lamp) {
     return <div>Lamp not found</div>;
   }
 
+  const handleGoBack = () => {
+    navigate('/catalog');
+  };
+
   return (
-    <div className="lamp-detail">
-      <img src={process.env.PUBLIC_URL + '/' + lamp.image} alt={lamp.title} width="500" height="350"/>
-      <div>
-        <h3>{lamp.title}</h3>
-        <p>{lamp.description}</p>
-        <p className="price">Price: {lamp.price} uah</p>
-        <div className="buttons">
-          <button disabled>Go Back</button>
+    <div className="lamp-detail-container">
+      <div className="lamp-detail">
+        <img src={process.env.PUBLIC_URL + '/' + lamp.image} alt={lamp.title} width="700" height="500" />
+        <div className="lamp-info">
+          <h3>{lamp.title}</h3>
+          <p>{lamp.description}</p>
+        </div>
+      </div>
+      <div className="lamp-actions">
+        <p className="lamp-price">Price: {lamp.price} uah</p>
+        <div className="action-buttons">
+        <button onClick={handleGoBack}>Go Back</button>
           <button disabled>Add to Cart</button>
         </div>
       </div>
