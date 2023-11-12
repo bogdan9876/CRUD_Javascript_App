6fb1ddd062abd4de934df1bb1a168c9393871f14
bogdan9876 <bogdankryveiko@gmail.com>
2023-11-12 14:04:08 +0200

    add demo view more

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,8 +1,8 @@
-function Button() {
+function Button({ onClick }) {
     return (
         <section className="button">
             <div className="view-more">
-                <button type="button" className="view-more-button"><a>View More</a></button>
+                <button type="button" className="view-more-button" onClick={onClick}><a>View More</a></button>
             </div>
         </section>
     );
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Section1 from '../Section1/section1';
 import '../../components/Section1/section1.css'
 import LampsSection from '../LampsSection/lampsSection';
@@ -6,13 +7,21 @@ import Button from '../Button/button';
 import '../../components/Button/button.css'
 
 function Home() {
+    const [sectionCount, setSectionCount] = useState(1);
+
+    const handleViewMore = () => {
+        setSectionCount(sectionCount + 1);
+    };
+
     return (
       <div className="Home">
         <Section1/>
-        <LampsSection/>
-        <Button/>
+        {[...Array(sectionCount)].map((_, index) => (
+          <LampsSection key={index} />
+        ))}
+        <Button onClick={handleViewMore}/>
       </div>
     );
-  }
+}
   
-  export default Home;
+export default Home;
