3a7fa8e3c3b41e3d5235c606ae63a3d8bca484a6
bogdan9876 <bogdankryveiko@gmail.com>
2023-11-12 19:59:28 +0200

    delete useless file

--- src/components/CatalogFilter/catalogFilter.js
+++ src/components/CatalogFilter/catalogFilter.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import './CatalogFilter.css';
 import SelectSort from './selectSort';
-import LampInput from './lampInput';
 
-const options = [
+const sortOptions = [
   { value: 'sortByPrice', label: 'Sort by price' },
   { value: 'sortByTitle', label: 'Sort by title' },
   { value: 'sortByType', label: 'Sort by type' },
 ];
 
+const idOptions = [
+  { value: '1', label: '1' },
+  { value: '2', label: '2' },
+  { value: '3', label: '3' },
+];
+
+const priceOptions = [
+  { value: '500', label: 'Up to 500 uah' },
+  { value: '1000', label: '501 - 1000 uah' },
+  { value: '1000+', label: '1000+ uah' },
+];
+
+
 const CatalogFilter = ({ onSortChange, onIdChange, onPriceChange }) => {
   return (
     <div className="wrapper">
       <div className="inner">
-        <SelectSort name="sort" options={options} onChange={onSortChange} />
-        <div>
-          <LampInput label="Lamp ID" name="LampID" onChange={onIdChange} />
-          <LampInput label="Lamp Price" name="LampPrice" onChange={onPriceChange} />
-        </div>
+        <SelectSort name="sort" options={sortOptions} onChange={onSortChange} />
+        <SelectSort name="id" options={idOptions} onChange={onIdChange} />
+        <SelectSort name="price" options={priceOptions} onChange={onPriceChange} />
         <button className="myButton">
           <span>Apply</span>
         </button>
