dfd7d0c074a0ca326021a080b4e9affd6205fa11
bogdan9876 <bogdankryveiko@gmail.com>
2023-11-18 14:05:36 +0200

    fix bug with catalog

--- src/components/Catalog/catalog.js
+++ src/components/Catalog/catalog.js
@@ -1,40 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import CatalogFilter from '../CatalogFilter/catalogFilter';
+import lamps from '../LampData/lampData';
 import './catalog.css';
 
 function Catalog({ searchTerm }) {
   const [filteredLamps, setFilteredLamps] = useState([]);
-  const [lamps, setLamps] = useState([
-    {
-      id: 1,
-      image: "images/section2(1).jpg",
-      title: " Electro1",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      price: 1000
-    },
-    {
-      id: 2,
-      image: "images/section2(2).jpg",
-      title: "Wood1",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      price: 1500
-    },
-    {
-      id: 3,
-      image: "images/section2(3).jpg",
-      title: "Wood2",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      price: 5000
-    },
-    {
-      id: 4,
-      image: "images/section2(4).jpg",
-      title: "Electro2",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      price: 1200
-    },
-  ]);
   const [sort, setSort] = useState('');
   const [idOption, setIdOption] = useState('');
   const [price, setPrice] = useState('');
