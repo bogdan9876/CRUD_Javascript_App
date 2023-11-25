bb57df29da93767ea8a72c9b6def34037983494f
bogdan9876 <bogdankryveiko@gmail.com>
2023-11-25 12:54:07 +0200

    next commit

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -5,10 +5,21 @@ const initialState = {
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return {
-        ...state,
-        items: [...state.items, { ...action.payload, quantity: 1 }],
-      };
+      const existingItem = state.items.find(item => item.id === action.payload.id);
+
+      if (existingItem) {
+        return {
+          ...state,
+          items: state.items.map(item =>
+            item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          items: [...state.items, { ...action.payload, quantity: 1 }],
+        };
+      }
     case 'REMOVE_FROM_CART':
       return {
         ...state,
@@ -21,24 +32,23 @@ const cartReducer = (state = initialState, action) => {
           item.id === action.payload ? { ...item, quantity: item.quantity + 1 } : item
         ),
       };
-      case 'DECREMENT_ITEM_QUANTITY':
-        return {
-          ...state,
-          items: state.items.map(item => {
-            if (item.id === action.payload) {
-              if (item.quantity === 1) {
-                return null;
-              } else {
-                return { ...item, quantity: item.quantity - 1 };
-              }
+    case 'DECREMENT_ITEM_QUANTITY':
+      return {
+        ...state,
+        items: state.items.map(item => {
+          if (item.id === action.payload) {
+            if (item.quantity === 1) {
+              return null;
+            } else {
+              return { ...item, quantity: item.quantity - 1 };
             }
-            return item;
-          }).filter(Boolean),
-        };
-        
-      default:
-        return state;
-    }
-  };
+          }
+          return item;
+        }).filter(Boolean),
+      };
+    default:
+      return state;
+  }
+};
 
 export default cartReducer;
