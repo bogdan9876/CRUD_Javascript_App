31ed261b03ffe53d94eeafc85299f8dd1bc0f0cb
bogdan9876 <bogdankryveiko@gmail.com>
2023-12-01 23:08:45 +0200

    demo formik

--- src/components/ErrorValid/errorValid.js
+++ src/components/ErrorValid/errorValid.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import { useFormikContext } from 'formik';
 
-
-const ErrorValid = () => {
-  const { errors } = useFormikContext();
-
-  const errorList = Object.keys(errors).map((fieldName, index) => (
-    <div key={index}>{errors[fieldName]}</div>
-  ));
-
-  return (
-    <div style={{ color: 'red' }}>
-      <p>Please correct the following errors:</p>
-      {errorList}
-    </div>
-  );
-};
+const ErrorValid = ({ children }) => (
+  <div style={{ color: 'red' }}>{children}</div>
+);
 
 export default ErrorValid;
