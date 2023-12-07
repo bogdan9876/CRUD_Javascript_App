f0cede9405af76a5fd0af1a8bdd76daab8dbba26
bogdan9876 <bogdankryveiko@gmail.com>
2023-12-07 16:27:08 +0200

    probably done

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -7,11 +7,16 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const validateEmail = (input) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(input);
+  };
+
   const handleLogin = () => {
     const registeredUser = localStorage.getItem('registeredUser');
     const registeredPassword = localStorage.getItem('registeredPassword');
 
-    if (email === registeredUser && password === registeredPassword) {
+    if (validateEmail(email) && email === registeredUser && password === registeredPassword) {
       localStorage.setItem('loggedInUser', email);
       navigate('/home');
     } else {
@@ -23,8 +28,8 @@ const Login = () => {
     <div className="login-container">
       <h2>Login</h2>
       <div className="input-container">
-        <input className="login-input" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input className="login-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <input className="login-input" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input className="login-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
       <button className="login-button" onClick={handleLogin}>Login</button>
       <p className='login-text'>Not a member? <span onClick={() => navigate('/register')}>Register</span></p>
--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -6,8 +6,22 @@ const Register = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [retypePassword, setRetypePassword] = useState('');
 
   const handleRegister = () => {
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+    
+    if (password !== retypePassword) {
+      alert("Passwords don't match");
+      return;
+    }
+
     localStorage.setItem('registeredUser', email);
     localStorage.setItem('registeredPassword', password);
     navigate('/home');
@@ -17,8 +31,10 @@ const Register = () => {
     <div className="register-container">
       <h2>Register</h2>
       <div className='register-container2'>
-      <input className="register-input" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      <input className="register-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <input className="register-input" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input className="register-input" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input className="register-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input className="register-input" type="password" placeholder="Retype Password" value={retypePassword} onChange={(e) => setRetypePassword(e.target.value)} />
       </div>
       <button className="register-button" onClick={handleRegister}>Register</button>
       <p className='register-text'>Already a member? <span onClick={() => navigate('/')}>Login</span></p>
